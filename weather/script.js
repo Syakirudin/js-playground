66568866b04418801f800ae8222dd6bab944552d
Syakirudin <36721931+Syakirudin@users.noreply.github.com>
2024-03-17 16:19:18 +0800

    CHANGE URL

--- weather/script.js
+++ weather/script.js
@@ -1,6 +1,7 @@
 
 const apiKey = '032004ca8f95408678aaea779051ed9c';
-const city = document.getElementById('city-name').value; 
+const city = 'london'; 
+
 
 // Fetch weather data from OpenWeatherMap API
 fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`)
@@ -10,12 +11,20 @@ fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}
   console.log(data);
   
   // Display the weather information
-//   const weatherDiv = document.getElementById('weather');
-//   weatherDiv.innerHTML = `
-//     <h2>${data.name}</h2>
-//     <p>Temperature: ${data.main.temp} Kelvin</p>
-//     <p>Description: ${data.weather[0].description}</p>
-//   `;
+  const weatherDiv = document.getElementById('weather');
+  const tempCelcius = data.main.temp -273.15;
+  const roundedTemp = tempCelcius.toFixed(1);
+
+  weatherDiv.innerHTML = `
+    <h2>${data.name}</h2>
+    <p>Temperature: ${roundedTemp} Celcius</p>
+    <p>Description: ${data.weather[0].description}</p>
+  `;
+
+  if (roundedTemp < 20) {
+    weatherDiv.innerHTML += '<p>🥶</p>';
+  }
+
 })
 .catch(error => {
   console.error('Error fetching weather data:', error);
