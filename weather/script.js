4cd488d62e1bcd03b269fc07bb1e4771d2825cf5
Syakirudin <36721931+Syakirudin@users.noreply.github.com>
2024-03-23 13:00:11 +0800

    update weather

--- weather/script.js
+++ weather/script.js
@@ -27,6 +27,7 @@ document.querySelectorAll('.city').forEach(cityDiv => {
     cityDiv.addEventListener('click', function() {
         let city = this.dataset.city; // Retrieve the city name from the data attribute
         let icon;
+        let emoji;
 
         fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`)
             .then(response => response.json())
@@ -41,31 +42,79 @@ document.querySelectorAll('.city').forEach(cityDiv => {
                 const tempCelcius = data.main.temp - 273.15;
                 const roundedTemp = tempCelcius.toFixed(1);
 
-                weatherDiv.innerHTML = `
-                    
-                    <p>Temperature: ${roundedTemp} Celsius</p>
-                    <p>Description: ${data.weather[0].description}</p>
-                    <p>${displayDateTime()}</p>
-                `;
-
-                header.innerHTML = `<h2>${data.name}</h2>`
-
                 if (roundedTemp >= 30) {
                     icon = '<img src="./src/sun-svgrepo-com.svg" alt="Sun" style="width: 300px; height: 300px;"/>';
+                    emoji = '🥵'
                 } else if (roundedTemp >= 20) {
                     icon = '<img src="./src/sun-behind-cloud-svgrepo-com.svg" alt="cloudy" style="width: 300px; height: 300px;">';
+                    emoji = '😊'
                 } else {
                     icon = '<img src="./src/sun-behind-rain-cloud-svgrepo-com.svg" alt="rain" style="width: 300px; height: 300px;">';
+                    emoji = '😌'
                 }
+                
+
+                weatherDiv.innerHTML = `
+                    
+                    <p>Temperature: ${roundedTemp} ${emoji} </p>
+                    <p>Description: ${data.weather[0].description}</p>
+                    <p>${displayDateTime()}</p>
+                    
+                `;
+
+                header.innerHTML = `<h2>${data.name}</h2>`             
+
                 
 
                 iconDiv.innerHTML = `<div class="icon-img">${icon}</div>`;
             })
             .catch(error => {
-                console.error('Error fetching weather data:', error);
+                console.error('Error fetching data:', error);
             });
+
     });
 });
+
+        document.querySelectorAll('.city').forEach(cityDiv => {
+            cityDiv.addEventListener('click', function() {
+                const cityName = this.textContent.trim(); // Get the text content of the clicked city
+
+                fetch(`https://waktu-solat-api.herokuapp.com/api/v1/prayer_times.json?zon=${cityName}`)
+                    .then(response => {
+                        if (!response.ok) {
+                            throw new Error('Network response was not ok');
+                        }
+                        return response.json();
+                    })
+                    .then(data => {
+                        // Display the entire data object in the console
+                        console.log(data);
+                    
+                        const waktuSolatDiv = document.getElementById('waktuSolat');
+                        const prayerTimes = data.data[0].waktu_solat;
+                    
+                        let prayerTimesHTML = '';
+                        prayerTimes.forEach(prayer => {
+                          prayerTimesHTML += `<div class="prayer-time">${prayer.name}: ${prayer.time}</div>`;
+                        });
+
+                        // Set the inner HTML of the element with the prayer times HTML
+                        waktuSolatDiv.innerHTML = `<ul>${prayerTimesHTML}</ul>`;
+                    })
+                    .catch(error => {
+                        console.error('Error fetching data:', error);
+                    });
+            });
+        });
+
+
+
+
+
+
+
+
+
 
 
 function displayDateTime() {
