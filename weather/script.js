24fc1c4d4003cf7d76a5c213174b880e9e2cc4b8
Syakirudin <36721931+Syakirudin@users.noreply.github.com>
2024-04-16 23:50:57 +0800

    test commit

--- weather/script.js
+++ weather/script.js
@@ -130,31 +130,42 @@ document.querySelectorAll(".city").forEach((cityDiv) => {
   });
 });
 
-function updateTime() {
-  // Create a new Date object to get the current time
-  const currentTime = new Date();
+function displayDateTime() {
+  let currentDate = new Date();
+  let date = currentDate.toDateString();
+  let time = currentDate.toLocaleTimeString();
 
-  // Extract hours, minutes, and seconds
-  const hours = currentTime.getHours();
-  const minutes = currentTime.getMinutes();
-  const seconds = currentTime.getSeconds();
-
-  // Add leading zeros if necessary
-  const formattedHours = (hours < 10 ? "0" : "") + hours;
-  const formattedMinutes = (minutes < 10 ? "0" : "") + minutes;
-  const formattedSeconds = (seconds < 10 ? "0" : "") + seconds;
-
-  // Format the time as HH:MM:SS
-  const formattedTime = formattedHours + ":" + formattedMinutes + ":" + formattedSeconds;
-
-  // Update the content of the paragraph with id "liveTime"
-  document.getElementById("liveTime").textContent = formattedTime;
+  return date + "<br>" + time;
 }
 
-// Call updateTime function initially to display the time immediately
-updateTime();
+// function updateTime() {
+//   // Create a new Date object to get the current time
+//   const currentTime = new Date();
 
-// Update time every second
-setInterval(updateTime, 1000);
+//   // Extract hours, minutes, and seconds
+//   const hours = currentTime.getHours();
+//   const minutes = currentTime.getMinutes();
+//   const seconds = currentTime.getSeconds();
+
+//   // Add leading zeros if necessary
+//   const formattedHours = (hours < 10 ? "0" : "") + hours;
+//   const formattedMinutes = (minutes < 10 ? "0" : "") + minutes;
+//   const formattedSeconds = (seconds < 10 ? "0" : "") + seconds;
+
+//   // Format the time as HH:MM:SS
+//   const formattedTime =
+//     formattedHours + ":" + formattedMinutes + ":" + formattedSeconds;
+
+//   // // Update the content of the paragraph with id "liveTime"
+//   // document.getElementById("liveTime").textContent = formattedTime;
+
+//   return formattedTime;
+// }
+
+// // Call updateTime function initially to display the time immediately
+// updateTime();
+
+// // Update time every second
+// setInterval(updateTime, 1000);
 
 document.getElementById("weather").innerHTML = displayDateTime();
